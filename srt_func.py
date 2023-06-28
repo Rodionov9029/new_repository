96f42260e00ee1cc519c0e2233b46a39ed2dcff8
Rodionov9029 <rodionovrenat@gmail.com>
2023-06-28 22:25:38 +0400

    add str_func

--- srt_func.py
+++ srt_func.py
@@ -1,2 +1,11 @@
-def str_ro_uppers(value):
+def str_to_uppers(value):
+    """
+    Переводит слова в верхний регистер
+    """
     return value.upper()
+
+def str_to_title(value):
+    """
+    Делает заглавными первые буквы слов
+    """
+    return value.title()
